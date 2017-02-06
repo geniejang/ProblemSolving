24624efc8103993883ea63d56c8eb12a5ced7970
Genie Jang <picatong11@gmail.com>
2017-02-06 23:47:56 +0900

    Problem #1. Two Sum
    https://leetcode.com/problems/two-sum/

---
+++ Leetcode/src/leetcode/no1_TwoSum/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.no1_TwoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			Integer smallIndex = map.get(target - nums[i]);
+			if (smallIndex == null) {
+				map.put(nums[i], i);
+			} else {
+				return new int[] { smallIndex, i };
+			}
+		}
+
+		return null;
+	}
+}

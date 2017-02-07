355659b35aeecd44cadff908a594214077e19f57
Genie Jang <picatong11@gmail.com>
2017-02-07 21:49:08 +0900

    Problem #6. Zig Zag Conversion
    https://leetcode.com/problems/zigzag-conversion/

---
+++ Leetcode/src/leetcode/no6_ZigZagConversion/Solution.java
@@ -0,0 +1,38 @@
+package leetcode.no6_ZigZagConversion;
+
+public class Solution {
+	public String convert(String s, int numRows) {
+		if (numRows == 1)
+			return s;
+
+		StringBuffer buffer = new StringBuffer(s.length());
+		buffer.setLength(s.length());
+		int mv1 = (numRows - 1) * 2;
+		int mv2 = 0;
+		int index = 0;
+		int row = 0;
+		for (int i = 0; i < s.length();) {
+			if (mv1 > 0) {
+				buffer.setCharAt(i++, s.charAt(index));
+				index += mv1;
+				if (index >= s.length()) {
+					index = ++row;
+					mv1 -= 2;
+					mv2 += 2;
+					continue;
+				}
+			}
+			if (mv2 > 0) {
+				buffer.setCharAt(i++, s.charAt(index));
+				index += mv2;
+				if (index >= s.length()) {
+					index = ++row;
+					mv1 -= 2;
+					mv2 += 2;
+				}
+			}
+		}
+
+		return buffer.toString();
+	}
+}
---
+++ Leetcode/src/leetcode/no6_ZigZagConversion/SolutionTest.java
@@ -0,0 +1,33 @@
+package leetcode.no6_ZigZagConversion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionTest {
+
+	private Solution solution;
+
+	@Before
+	public void setUp() throws Exception {
+		solution = new Solution();
+	}
+
+	@Test
+	public void convertSingleChar() {
+		Solution solution = new Solution();
+		String actual = solution.convert("A", 1);
+		String expected = "A";
+		assertTrue(expected.equals(actual));
+	}
+
+	@Test
+	public void convertExample() {
+		Solution solution = new Solution();
+		String actual = solution.convert("PAYPALISHIRING", 3);
+		String expected = "PAHNAPLSIIGYIR";
+		assertTrue(expected.equals(actual));
+	}
+
+}

30d5ce4112bcda2dfc8d8e5b4d7ad754ba19ef74
Genie Jang <picatong11@gmail.com>
2017-03-11 11:34:59 +0900

    Problem #11. Container With Most Water

---
+++ Leetcode/src/leetcode/no11_ContainerWithMostWater/SolutionTest.java
@@ -0,0 +1,30 @@
+package leetcode.no11_ContainerWithMostWater;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionTest {
+
+	private Solution solution;
+
+	@Before
+	public void setUp() throws Exception {
+		solution = new Solution();
+	}
+
+	@Test
+	public void initiated() {
+		assertNotNull(solution);
+	}
+
+	@Test
+	public void test() {
+		int[] height = { 1, 3, 2, 3, 1 };
+		int expected = 6;
+		int actual = solution.maxArea(height);
+		assertEquals(expected, actual);
+	}
+
+}

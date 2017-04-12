169a024e2f92b6163795c764fea195988cab92df
Genie Jang <picatong11@gmail.com>
2017-04-13 00:15:58 +0900

    Problem #5. Longest Palindrome Substring

---
+++ Leetcode/src/leetcode/no5_LongestPalindromeSubstring/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.no5_LongestPalindromeSubstring;
+
+public class Solution {
+	public String longestPalindrome(String s) {
+		boolean[][] dp = new boolean[s.length()][s.length()];
+		int longest = 0;
+		int left = 0;
+		int right = 0;
+		for (int r = 0; r < s.length(); r++) {
+			for (int l = 0; l <= r; l++) {
+				if (s.charAt(l) == s.charAt(r) && (l + 1 > r - 1 || dp[l + 1][r - 1])) {
+					dp[l][r] = true;
+					if (longest < r - l + 1) {
+						longest = r - l + 1;
+						left = l;
+						right = r;
+					}
+				}
+			}
+		}
+		return s.substring(left, right + 1);
+	}
+}

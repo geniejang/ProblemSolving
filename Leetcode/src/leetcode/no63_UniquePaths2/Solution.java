fb45ce44867aba379a849146cb468bb6ad82baf6
Genie Jang <picatong11@gmail.com>
2017-04-15 00:17:04 +0900

    Problem #63. Unique Paths II

---
+++ Leetcode/src/leetcode/no63_UniquePaths2/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.no63_UniquePaths2;
+
+public class Solution {
+	public int uniquePathsWithObstaclesOptimized(int[][] obstacleGrid) {
+		int[] paths = new int[obstacleGrid[0].length];
+		paths[0] = 1;
+		for (int[] row : obstacleGrid) {
+			for (int c = 0; c < row.length; c++) {
+				if (row[c] == 1) {
+					paths[c] = 0;
+				} else if (c > 0) {
+					paths[c] += paths[c - 1];
+				}
+			}
+		}
+		return paths[paths.length - 1];
+	}
+}

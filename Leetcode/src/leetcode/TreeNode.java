b903e686dd4be7d00dbf76c13988b6c0f14af2d2
Genie Jang <picatong11@gmail.com>
2017-04-17 22:13:06 +0900

    Add methods to make testing and debugging easier.
    * TreeNode.java
    	- equals()
    		- Compare instances by their values
    		- Order by the value of root, left, and right
    		- Ascending order
    	- from()
    		- Convert Integer array to Tree
    		- Takes the wrapper type Integer[] to accomodate "null" children
    	- toString(): describe TreeNodes for debugging purpose
    	- COMPARATOR: static member to be used in Collections.sort() and
    Arrays.sort()
    
    * TreeNodeTest.java
    	- Implement tests for new methods and COMPARATOR
    	- Test equals() without from() to test equals() first

--- Leetcode/src/leetcode/TreeNode.java
+++ Leetcode/src/leetcode/TreeNode.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Comparator;
+
 public class TreeNode {
 	public int val;
 	public TreeNode left;
@@ -8,4 +10,66 @@ public class TreeNode {
 	public TreeNode(int x) {
 		val = x;
 	}
+
+	public static TreeNode from(Integer[] arr) {
+		if (arr == null || arr.length == 0) {
+			return null;
+		}
+		return from(arr, 0);
+	}
+
+	private static TreeNode from(Integer[] arr, int index) {
+		TreeNode node = null;
+		if (index < arr.length && arr[index] != null) {
+			node = new TreeNode(arr[index]);
+			node.left = from(arr, index * 2 + 1);
+			node.right = from(arr, index * 2 + 2);
+		}
+		return node;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (false == obj instanceof TreeNode) {
+			return false;
+		}
+		TreeNode node = (TreeNode) obj;
+		if (val != node.val)
+			return false;
+
+		if (left == null && node.left != null)
+			return false;
+		else if (left != null && !left.equals(node.left))
+			return false;
+
+		if (right == null && node.right != null)
+			return false;
+		else if (right != null && !right.equals(node.right))
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + val + ", L:" + left + ", R:" + right + "]";
+	}
+
+	public static Comparator<TreeNode> COMPARATOR = new Comparator<TreeNode>() {
+		@Override
+		public int compare(TreeNode o1, TreeNode o2) {
+			if (o1 == null && o2 == null)
+				return 0;
+			if (o1 == null)
+				return 1;
+			if (o2 == null)
+				return -1;
+			int diff = o1.val - o2.val;
+			if (diff == 0)
+				diff = compare(o1.left, o2.left);
+			if (diff == 0)
+				diff = compare(o1.right, o2.right);
+			return diff;
+		}
+	};
 }
---
+++ Leetcode/src/leetcode/TreeNodeTest.java
@@ -0,0 +1,100 @@
+package leetcode;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TreeNodeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void equalsAgainstSameValuedNodes() {
+		TreeNode n1 = new TreeNode(1);
+		TreeNode n2 = new TreeNode(1);
+		assertEquals(n2, n1);
+	}
+
+	@Test
+	public void equalsAgainstDifferentValuedNodes() {
+		TreeNode n1 = new TreeNode(1);
+		TreeNode n2 = new TreeNode(2);
+		assertNotEquals(n2, n1);
+	}
+
+	@Test
+	public void euqlasAgainstSameValuedChildren() {
+		TreeNode n1 = new TreeNode(1);
+		n1.left = new TreeNode(3);
+		n1.right = new TreeNode(8);
+		TreeNode n2 = new TreeNode(1);
+		n2.left = new TreeNode(3);
+		n2.right = new TreeNode(8);
+		assertEquals(n2, n1);
+	}
+
+	@Test
+	public void euqlasAgainstDifferentValuedChildren() {
+		TreeNode n1 = new TreeNode(1);
+		n1.left = new TreeNode(3);
+		n1.right = new TreeNode(8);
+		TreeNode n2 = new TreeNode(1);
+		n2.left = new TreeNode(5);
+		n2.right = new TreeNode(8);
+		assertNotEquals(n2, n1);
+	}
+
+	@Test
+	public void fromIntegerArray() {
+		TreeNode expected = new TreeNode(0);
+		expected.left = new TreeNode(1);
+		expected.right = new TreeNode(2);
+		expected.left.left = new TreeNode(3);
+		expected.left.right = new TreeNode(4);
+		expected.right.right = new TreeNode(6);
+		expected.left.left.right = new TreeNode(8);
+		TreeNode actual = TreeNode.from(new Integer[] { 0, 1, 2, 3, 4, null, 6, null, 8 });
+		assertEquals(expected, actual);
+	}
+
+	private void assertSort(TreeNode n1, TreeNode n2) {
+		List<TreeNode> list1 = new ArrayList<>();
+		list1.add(n1);
+		list1.add(n2);
+		Collections.sort(list1, TreeNode.COMPARATOR);
+		List<TreeNode> list2 = new LinkedList<>();
+		list2.add(n2);
+		list2.add(n1);
+		Collections.sort(list2, TreeNode.COMPARATOR);
+		assertEquals(list1, list2);
+	}
+
+	@Test
+	public void sortSingleNodes() {
+		TreeNode n1 = new TreeNode(1);
+		TreeNode n2 = new TreeNode(3);
+		assertSort(n1, n2);
+	}
+
+	@Test
+	public void sortTreesSameRootDifferentLeft() {
+		TreeNode t1 = TreeNode.from(new Integer[] { 3, 2 });
+		TreeNode t2 = TreeNode.from(new Integer[] { 3, 1 });
+		assertSort(t1, t2);
+	}
+
+	@Test
+	public void sortTreesSameRootSameLeftDifferentRight() {
+		TreeNode t1 = TreeNode.from(new Integer[] { 3, 2, 5 });
+		TreeNode t2 = TreeNode.from(new Integer[] { 3, 2, 7 });
+		assertSort(t1, t2);
+	}
+}

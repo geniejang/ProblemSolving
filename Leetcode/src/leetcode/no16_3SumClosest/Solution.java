44c8819a7da6dca1e138adde717894331911d899
Genie Jang <picatong11@gmail.com>
2017-05-13 22:28:15 +0900

    Problem #16. 3 Sum Closest

---
+++ Leetcode/src/leetcode/no16_3SumClosest/Solution.java
@@ -0,0 +1,47 @@
+package leetcode.no16_3SumClosest;
+
+import java.util.Arrays;
+
+public class Solution {
+	public int threeSumClosest(int[] nums, int target) {
+		if (nums.length < 3) {
+			return -1;
+		}
+		Arrays.sort(nums);
+		int closest = Integer.MAX_VALUE;
+		int diff = Integer.MAX_VALUE;
+		for (int i = 0; i < nums.length - 2; i++) {
+			int l = i + 1;
+			int r = nums.length - 1;
+			int threeSum = nums[i] + nums[l] + nums[r];
+			while (l < r) {
+				if (threeSum == target) {
+					return threeSum;
+				}
+				int newDiff = Integer.max(target - threeSum, threeSum - target);
+				if (newDiff < diff) {
+					closest = threeSum;
+					diff = newDiff;
+				}
+				if (threeSum < target) {
+					threeSum -= nums[l];
+					do {
+						l++;
+					} while (l < r && nums[l - 1] == nums[l]);
+					threeSum += nums[l];
+				}
+				if (threeSum > target) {
+					threeSum -= nums[r];
+					do {
+						r--;
+					} while (l < r && nums[r] == nums[r + 1]);
+					threeSum += nums[r];
+				}
+			}
+			while (i < nums.length - 2 && nums[i] == nums[i + 1]) {
+				i++;
+			}
+		}
+		return closest;
+	}
+}
---
+++ Leetcode/src/leetcode/no16_3SumClosest/SolutionTest.java
@@ -0,0 +1,73 @@
+package leetcode.no16_3SumClosest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionTest {
+
+	private Solution solution;
+
+	@Before
+	public void setUp() throws Exception {
+		solution = new Solution();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		solution = null;
+	}
+
+	@Test
+	public void initiated() {
+		assertNotNull(solution);
+	}
+
+	private void assertThreeSumClosest(int expected, int target, int[] nums) {
+		int actual = solution.threeSumClosest(nums, target);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void test() {
+		int[] nums = { -1, 2, 1, -4 };
+		int target = 1;
+		int expected = 2;
+		assertThreeSumClosest(expected, target, nums);
+	}
+
+	@Test
+	public void zeroOneTwo() {
+		int[] nums = { 0, 1, 2 };
+		int target = 3;
+		int expected = 3;
+		assertThreeSumClosest(expected, target, nums);
+	}
+
+	@Test
+	public void onePositive() {
+		int[] nums = { -3, -2, -5, 3, -4 };
+		int target = -1;
+		int expected = -2;
+		assertThreeSumClosest(expected, target, nums);
+	}
+
+	@Test
+	public void oneNegative() {
+		int[] nums = { 0, 2, 1, -3 };
+		int target = 1;
+		int expected = 0;
+		assertThreeSumClosest(expected, target, nums);
+	}
+
+	@Test
+	public void oneAll() {
+		int[] nums = { 1, 1, 1, 1 };
+		int target = 0;
+		int expected = 3;
+		assertThreeSumClosest(expected, target, nums);
+	}
+
+}

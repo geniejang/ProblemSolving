5052caf1f2e9810bbb81835e4fe7be89bad2206a
Genie Jang <picatong11@gmail.com>
2017-07-10 22:29:18 +0900

    Add the template codes for Leetcode.

---
+++ Leetcode/src/leetcode/SolutionTest.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionTest {
+
+	private Solution solution;
+
+	@Before
+	public void setUp() throws Exception {
+		solution = new Solution();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		solution = null;
+	}
+
+	@Test
+	public void initiated() {
+		assertNotNull(solution);
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

2c6e49a4b62062d7c9bd7d7bb3ed57af97afbffa
Genie Jang <picatong11@gmail.com>
2018-02-24 17:06:01 +0900

    Problem #413: Refactoring.
    * Solution.java
    	- Update the number of partial arithmetic slices instead of
    combination.

--- Leetcode/src/leetcode/no413_ArithmeticSlices/Solution.java
+++ Leetcode/src/leetcode/no413_ArithmeticSlices/Solution.java
@@ -6,21 +6,15 @@ public class Solution {
 			return 0;
 		}
 		int diff = A[1] - A[0];
-		int idx = 0;
 		int cnt = 0;
-		for (int i = 1; i + 1 < A.length; i++) {
-			if (diff != A[i + 1] - A[i]) {
-				diff = A[i + 1] - A[i];
-				int n = i - idx;
-				if (n >= 2) {
-					cnt += n * (n - 1) / 2;
-				}
-				idx = i;
+		int n = 0;
+		for (int i = 2; i < A.length; i++) {
+			if (diff == A[i] - A[i - 1]) {
+				cnt += ++n;
+			} else {
+				diff = A[i] - A[i - 1];
+				n = 0;
 			}
-		}
-		if (idx + 2 < A.length) {
-			int n = A.length - 1 - idx;
-			cnt += n * (n - 1) / 2;
 		}
 		return cnt;
 	}

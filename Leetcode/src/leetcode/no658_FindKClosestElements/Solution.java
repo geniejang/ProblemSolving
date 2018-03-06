63fb707121ad504ba16adf23372d6d5166614692
Genie Jang <picatong11@gmail.com>
2018-03-06 22:45:17 +0900

    Problem #658: Improve the solution.
    * Solution.java
    	- Search in n-k elements
    	- Compare the center & center+k
    	- in a binary search loop

--- Leetcode/src/leetcode/no658_FindKClosestElements/Solution.java
+++ Leetcode/src/leetcode/no658_FindKClosestElements/Solution.java
@@ -5,41 +5,20 @@ import java.util.List;
 
 public class Solution {
 	public List<Integer> findClosestElements(int[] arr, int k, int x) {
-		int i = bsearch(arr, x);
-		int l = i;
-		int r = i + 1;
-		while (r - l < k) {
-			if (l == 0) {
-				r++;
-			} else if (r == arr.length) {
-				l--;
-			} else if (x - arr[l - 1] <= arr[r] - x) {
-				l--;
+		int l = 0;
+		int r = arr.length - k;
+		while (l < r) {
+			int c = (l + r) >> 1;
+			if (x - arr[c] > arr[c + k] - x) {
+				l = c + 1;
 			} else {
-				r++;
+				r = c;
 			}
 		}
 		List<Integer> list = new ArrayList<>(k);
-		for (int idx = l; idx < r; idx++) {
-			list.add(arr[idx]);
+		for (int i = 0; i < k; i++) {
+			list.add(arr[l + i]);
 		}
 		return list;
-	}
-
-	private int bsearch(int[] arr, int x) {
-		int l = 0;
-		int r = arr.length - 1;
-		while (l < r) {
-			int c = (l + r) >> 1;
-			int diff = arr[c] - x;
-			if (diff == 0) {
-				return c;
-			} else if (diff < 0) {
-				l = c + 1;
-			} else {
-				r = c - 1;
-			}
-		}
-		return l;
 	}
 }

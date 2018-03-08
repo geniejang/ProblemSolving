ab0fdaaf30c9e32feab85af83a7664241e75411c
Genie Jang <picatong11@gmail.com>
2018-03-08 22:24:46 +0900

    Problem #436: Add another solution with TreeMap.

--- Leetcode/src/leetcode/no436_FindRightInterval/Solution.java
+++ Leetcode/src/leetcode/no436_FindRightInterval/Solution.java
@@ -3,6 +3,7 @@ package leetcode.no436_FindRightInterval;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 import leetcode.Interval;
 
@@ -35,5 +36,18 @@ public class Solution {
 			}
 		}
 		return null;
+	}
+
+	public int[] findRightIntervalTreeMap(Interval[] intervals) {
+		int[] result = new int[intervals.length];
+		java.util.NavigableMap<Integer, Integer> intervalMap = new TreeMap<>();
+		for (int i = 0; i < intervals.length; ++i) {
+			intervalMap.put(intervals[i].start, i);
+		}
+		for (int i = 0; i < intervals.length; ++i) {
+			Map.Entry<Integer, Integer> entry = intervalMap.ceilingEntry(intervals[i].end);
+			result[i] = (entry != null) ? entry.getValue() : -1;
+		}
+		return result;
 	}
 }
